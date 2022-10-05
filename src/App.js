02569948ce5d6957f57ad0e5b12901ce5a77cce8
Chanwoo <tree698@gmail.com>
2022-10-05 15:36:02 +0900

    add css global setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Let's Start!</h1>
+      <img style={{ width: '1100px' }} src="images/me4.png" alt="" />
+      <img style={{ width: '800px' }} src="images/me5.png" alt="" />
+      <img style={{ width: '800px' }} src="images/volunteer.png" alt="" />
     </div>
   );
-}
+};
 
 export default App;
