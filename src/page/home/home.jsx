9b3985894646f43e5c2712b429daec86f4adc7d9
Chanwoo <tree698@gmail.com>
2022-10-10 19:50:25 +0900

    HTML: Home

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -6,11 +6,23 @@ import styles from './home.module.css';
 const Home = (props) => {
   return (
     <section className={styles.container}>
-      <div className={styles.home}>
-        <Header addable={true} />
-        <main className={styles.main}></main>
-        <Footer />
-      </div>
+      <Header addable={true} />
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h3>Hello I'm</h3>
+            <h1>CHANWOO LEE</h1>
+            <h2>A reliable Web Programmer</h2>
+          </div>
+          <div className={styles.banner}>
+            <div>Front-end + Back-end</div>
+            <div>+14 Projects</div>
+            <div>clean code</div>
+          </div>
+        </div>
+        <img className={styles.image} src="images/me5.png" alt="" />
+      </main>
+      <Footer />
     </section>
   );
 };
