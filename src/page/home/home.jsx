4aa724bd3799fb351daa70ba8131d0e880d1854f
Chanwoo <tree698@gmail.com>
2022-10-15 21:04:31 +0900

    add responsive: home > main

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -20,57 +20,58 @@ const Home = (props) => {
   return (
     <section className={styles.container}>
       <Header addable={true} />
-      {/* <main className={styles.main}>
+      <main className={styles.main}>
         <div className={styles.content}>
           <div className={styles.title}>
             <h2 className={styles.hello}>Hello I'm</h2>
             <h1 className={styles.name}>CHANWOO LEE</h1>
-            <h2 className={styles.description}>
+            <h3 className={styles.description}>
               A reliable Web ProgrammerA reliable Web ProgrammerA reliable Web
               ProgrammerA reliable Web Programmer ProgrammerA reliable Web
               Programmer
-            </h2>
+            </h3>
           </div>
           <div className={styles.items}>
             <button className={styles.item} onClick={onGoToPortfolio}>
               <div className={styles.itemImg_wrap}>
                 <img className={styles.itemImg} src="images/logo1.png" alt="" />
               </div>
-
-              <h2 className={styles.itemTitle}>Full Stack</h2>
-              <p className={styles.itemDescript}>
-                A reliable Web ProgrammerA reliable Web ProgrammerA reliable Web
-                ProgrammerA
-              </p>
+              <div className={styles.itemInfo_wrap}>
+                <h2 className={styles.itemTitle}>Full Stack</h2>
+                <p className={styles.itemDescript}>
+                  A reliable Web ProgrammerA reliable Web ProgrammerA reliable
+                  Web ProgrammerA
+                </p>
+              </div>
             </button>
             <button className={styles.item} onClick={onGoToProjects}>
               <div className={styles.itemImg_wrap}>
-                <img
-                  className={styles.itemImg}
-                  src="images/funGame.png"
-                  alt=""
-                />
+                <img className={styles.itemImg} src="images/logo1.png" alt="" />
               </div>
-              <h2 className={styles.itemTitle}>14+ Side-Projects</h2>
-              <p className={styles.itemDescript}>
-                A reliable Web ProgrammerA reliable Web ProgrammerA reliable Web
-                ProgrammerA
-              </p>
+              <div className={styles.itemInfo_wrap}>
+                <h2 className={styles.itemTitle}>14+ Side-Projects</h2>
+                <p className={styles.itemDescript}>
+                  A reliable Web ProgrammerA reliable Web ProgrammerA reliable
+                  Web ProgrammerA
+                </p>
+              </div>
             </button>
             <button className={styles.item} onClick={onGoToAbout}>
               <div className={styles.itemImg_wrap}>
                 <img className={styles.itemImg} src="images/logo1.png" alt="" />
               </div>
-              <h2 className={styles.itemTitle}>Clean Code</h2>
-              <p className={styles.itemDescript}>
-                A reliable Web ProgrammerA reliable Web ProgrammerA reliable Web
-                ProgrammerA
-              </p>
+              <div className={styles.itemInfo_wrap}>
+                <h2 className={styles.itemTitle}>Clean Code</h2>
+                <p className={styles.itemDescript}>
+                  A reliable Web ProgrammerA reliable Web ProgrammerA reliable
+                  Web ProgrammerA
+                </p>
+              </div>
             </button>
           </div>
         </div>
-        <img className={styles.image} src="images/me5.png" alt="" />
-      </main> */}
+        <img className={styles.image} src="images/me.png" alt="" />
+      </main>
       <Footer />
     </section>
   );
