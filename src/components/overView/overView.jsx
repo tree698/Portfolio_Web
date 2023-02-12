8907cfac53836a27d108c522a0b945228dd64060
Chanwoo <tree698@gmail.com>
2023-02-12 16:30:16 +0900

    RE-setup project

--- src/components/overView/overView.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './overView.module.css';
-
-const OverView = (props) => <h1>Overview</h1>;
-
-export default OverView;
--- src/components/techStack/techStack.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './techStack.module.css';
-
-const TechStack = (props) => <h1>TechStack</h1>;
-
-export default TechStack;
